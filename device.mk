46276ad5144d5450b0d68a9523489efd4e0d3bdd
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    gauguin: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+$(call soong_config_set,camera,override_format_from_reserved,true)
 $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_gauguin)
 
 PRODUCT_PACKAGES += \
