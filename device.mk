afc9bf4c72b43874b7175429becf76e57ceae04b
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-01 03:14:39 +0545

    gauguin: Set libcameraservice ext lib via soong config
    Change-Id: I2bb052c75d9a7765a554372be1eceba25e324402

--- device.mk
+++ device.mk
@@ -121,6 +121,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.xiaomi_gauguin)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
